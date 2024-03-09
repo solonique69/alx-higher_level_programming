99124cc71c253b7ec45ebe085c45db4b0b5aa856
Ovie Okomowho <solonique2020@gmail.com>
2024-03-09 19:18:28 +0100

    file: Created 1-last_digit.py file - task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+
+def _abs(num):
+    if num < 0:
+        return -num
+    else:
+        return num
+
+
+remainder = (_abs(number) % 10)
+
+if number < 0:
+    remainder = -1 * remainder
+
+
+print(f"Last digit of {number} is {remainder}", end=" ")
+if remainder > 5:
+    print("and is greater than 5")
+elif remainder == 0:
+    print("and is 0")
+elif remainder < 6 and remainder != 0:
+    print("is less than 6 and not 0")
