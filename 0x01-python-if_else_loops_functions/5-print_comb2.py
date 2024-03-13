885a2590d98287fb767f3b3fc8e31a69716f3cee
Ovie Okomowho <solonique2020@gmail.com>
2024-03-13 01:35:30 +0100

    edited repo

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+for x in range(0, 10):
+    print("{}{}".format(x), end=", ")
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Script creates a table with parameters in a database on MySQL server
+CREATE TABLE IF NOT EXISTS first_table
+(
+	id INT,
+	name VARCHAR(256)
+);
